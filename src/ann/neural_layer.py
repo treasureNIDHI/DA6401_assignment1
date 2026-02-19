45be9f203424ef5b7efa3a04601260736b8d1199
Nidhi Kumari <94866932+treasureNIDHI@users.noreply.github.com>
2026-02-19 13:26:17 +0530

    neural layers and objective functions are updated

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -2,3 +2,38 @@
 Neural Layer Implementation
 Handles weight initialization, forward pass, and gradient computation
 """
+import numpy as np
+
+class Dense:
+    def __init__(self, input_features, output_features, init_method="random" ):
+        if init_method == "random":
+            self.W = np.random.randn(input_features, output_features) * 0.01
+        elif init_method == "xavier":
+            limit = np.sqrt(6 / (input_features + output_features))
+            self.W = np.random.uniform(-limit, limit, (input_features, output_features))
+        else:
+            raise ValueError("Invalid initialization method")
+        
+        self.b = np.zeros((1, output_features))
+        
+        # Gradients (required by autograder)
+        self.grad_W = np.zeros_like(self.W)
+        self.grad_b = np.zeros_like(self.b)
+        
+        # Cache for backward pass
+        self.cache_x = None
+
+    def forward(self, x):
+        self.cache_x = x
+        return np.dot(x, self.W) + self.b
+    
+    def backward(self, dout):
+
+        batch_size = self.cache_x.shape[0]
+        self.grad_W = np.dot(self.cache_x.T, dout) / batch_size
+        self.grad_b = np.sum(dout, axis=0, keepdims=True) / batch_size
+        grad_input = np.dot(dout, self.W.T)
+        return grad_input
+        
+
+    
--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -2,3 +2,43 @@
 Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
+import numpy as np
+
+class CrossEntropyLoss:
+
+    def forward(self, y_pred, y_true):
+
+        self.y_pred = y_pred
+        self.y_true = y_true
+
+        loss = -np.mean(np.sum(y_true * np.log(y_pred + 1e-15), axis=1))
+
+        return loss
+
+
+    def backward(self):
+
+        gradient = (self.y_pred - self.y_true) / self.y_true.shape[0]
+
+
+        return gradient
+
+
+class MSELoss:
+
+    def forward(self, y_pred, y_true):
+
+        self.y_pred = y_pred
+        self.y_true = y_true
+
+        loss = np.mean((y_pred - y_true) ** 2)
+
+        return loss
+
+
+    def backward(self):
+
+        gradient = 2 * (self.y_pred - self.y_true) / self.y_true.shape[0]
+        
+
+        return gradient
